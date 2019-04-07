5215b1b130bd75bb65014eaaff8017bab92f61e7
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-04-07 17:53:04 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openmenu(){
+  document.getElementById("side-bar").style.display="block";
+  document.getElementById("menu-btn").style.display="none";
+  document.getElementById("close-btn").style.display="block";
+}
+function closemenu(){
+  document.getElementById("side-bar").style.display="none";
+  document.getElementById("menu-btn").style.display="block";
+  document.getElementById("close-btn").style.display="none";
+}
