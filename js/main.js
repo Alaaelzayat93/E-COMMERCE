9bd42978c10d4dfc12ae27e5211ef90124ffe05d
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-04-09 16:20:24 +0200

    new

--- js/main.js
+++ js/main.js
@@ -19,13 +19,27 @@ navbar.removeClass('scrolled navbar-fixed-top');
 }
 });
 /*login*/
- $("#myModal").validate({
-  submitHandler: function(form) {
-    // some other code
-    // maybe disabling submit button
-    // then:
-    $(form).submit();
-  }
- });
+ 
+            $.validator.addMethod("EMAIL", function(value, element) {
+                return this.optional(element) || /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/i.test(value);
+            }, "Email Address is invalid: Please enter a valid email address.");
+
+            $.validator.addMethod("PASSWORD",function(value,element){
+                return this.optional(element) || /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/i.test(value);
+            },"Passwords are 8-16 characters with uppercase letters, lowercase letters and at least one number.");
+
+            $.validator.addMethod("SUBMIT",function(value,element){
+                return this.optional(element) || /[^ ]/i.test(value);
+            },"You did not click the submit button.");
+
+            // Validate signup form on keyup and submit
+            $("#LOGIN").validate({
+                rules: {
+                    EMAIL: "required EMAIL",
+                    PASSWORD: "required PASSWORD",
+                    SUBMIT: "required SUBMIT",
+                },
+            });
+        
 
 
